e59f6fcfea16d2733c77cb1c4db4958a95bdf22d
S0k1l <martynuky16@gmail.com>
2024-11-23 22:37:55 +0200

    Angular 18 User Registration with Asp.Net Core Web API

---
+++ AuthECClient/src/app/shared/services/auth.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+  baseUrl = 'http://localhost:5129/api';
+
+  createUser(formData: any) {
+    return this.http.post(this.baseUrl + '/signup', formData);
+  }
+}
---
+++ AuthECClient/src/app/user/registration/registration.component.ts
@@ -0,0 +1,103 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { FirstKeyPipe } from '../../shared/pipes/first-key.pipe';
+import { AuthService } from '../../shared/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, FirstKeyPipe],
+  templateUrl: './registration.component.html',
+  styleUrl: './registration.component.css',
+})
+export class RegistrationComponent {
+  constructor(
+    public formBuilder: FormBuilder,
+    public authService: AuthService,
+    public toastr: ToastrService
+  ) {}
+
+  isSubmitted: boolean = false;
+
+  passwordMatchValidator: ValidatorFn = (control: AbstractControl): null => {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+
+    if (password && confirmPassword && password.value != confirmPassword.value)
+      confirmPassword?.setErrors({ passwordMatch: true });
+    else confirmPassword?.setErrors(null);
+
+    return null;
+  };
+
+  form = this.formBuilder.group(
+    {
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(/(?=.*[^a-zA-Z0-9 ])/),
+        ],
+      ],
+      confirmPassword: ['', Validators.required],
+    },
+    {
+      validators: this.passwordMatchValidator,
+    }
+  );
+
+  onSubmit() {
+    this.isSubmitted = true;
+    if (this.form.valid) {
+      this.authService.createUser(this.form.value).subscribe({
+        next: (res: any) => {
+          if (res.succeeded) {
+            this.form.reset();
+            this.isSubmitted = false;
+            this.toastr.success('New user created', 'Registration Successful');
+          }
+        },
+        error: (err) => {
+          if (err.error.errors)
+            err.error.errors.forEach((x: any) => {
+              switch (x.code) {
+                case 'DuplicateUserName':
+                  break;
+                case 'DuplicateEmail':
+                  this.toastr.error(
+                    'Email is already taken',
+                    'Registration Failed'
+                  );
+                  break;
+                default:
+                  this.toastr.error('Contact developer', 'Registration Failed');
+                  console.log(x);
+                  break;
+              }
+            });
+          else console.log('error:', err);
+        },
+      });
+    }
+  }
+
+  hasDisplayableError(controlName: string): boolean {
+    const control = this.form.get(controlName);
+    return (
+      Boolean(control?.invalid) &&
+      (this.isSubmitted || Boolean(control?.touched) || Boolean(control?.dirty))
+    );
+  }
+}
